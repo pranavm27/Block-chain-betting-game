8db849f7a5b549044e7fd015986d38ea1490af3c
Pranav M <m.pranav@ymail.com>
2022-11-30 14:31:07 +0530

    build error fix

--- src/components/wallet/connectors.tsx
+++ src/components/wallet/connectors.tsx
@@ -1,4 +1,4 @@
-import { CHAIN_ID} from "config/config";
+import { CHAIN_ID} from "@/config/config";
 import { InjectedConnector } from '@web3-react/injected-connector'
 
 export const injected = new InjectedConnector({ supportedChainIds: [CHAIN_ID] })
